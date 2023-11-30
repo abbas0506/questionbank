bde4e97c7d2ff7c497303c20bfacb3feeb3a63cb
abbas0506 <abbas.sscs@gmail.com>
2023-11-30 16:43:20 +0500

    30112309

--- resources/views/modules/library/assistant/qrcodes/books/preview.blade.php
+++ resources/views/modules/library/assistant/qrcodes/books/preview.blade.php
@@ -101,8 +101,8 @@ $roman = config('global.romans');
                                 <td>{{$book->rack->label}}</td>
                                 <td class="w-20 p-2">
                                     <div class="flex flex-col">
-                                        <!-- <div>{!! DNS2D::getBarcodeHTML($book->reference()."-".$copy_sr, 'QRCODE',4,4) !!}</div> -->
-                                        <div>{!! DNS1D::getBarcodeHTML($book->reference()."-".$copy_sr, 'C39',0.6,40) !!}</div>
+                                        <div>{!! DNS2D::getBarcodeHTML($book->reference()."-".$copy_sr, 'QRCODE',4,4) !!}</div>
+                                        <!-- <div>{!! DNS1D::getBarcodeHTML($book->reference()."-".$copy_sr, 'C39',0.6,40) !!}</div> -->
                                         <label class="text-xs">{{$book->reference()}}-{{$copy_sr}}</label>
                                     </div>
 
